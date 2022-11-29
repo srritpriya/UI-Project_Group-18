458adbbf60a550a9d0b0ea6556ff6711f9b2defa
srritpriya <91395762+srritpriya@users.noreply.github.com>
2022-11-29 16:59:02 -0500

    Add files via upload

---
+++ aboutUs.php
@@ -0,0 +1,49 @@
+
+<?php include 'header.php';?>
+<section class="breadcrumb_area breadcrumb1">
+    <div class="container">
+       <div class="row">
+          <div class="col-md-12">
+             <div class="breadcrumb_wrapper d-flex align-items-center justify-content-between flex-wrap">
+                <h4 class="page_title">About Us</h4>
+                <nav aria-label="breadcrumb">
+                   <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">About Us</li>
+
+                   </ol>
+                </nav>
+             </div>
+          </div>
+       </div>
+       <!-- ends: .row -->
+    </div>
+</section>
+<section class="pt-5">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-3 col-md-6">
+                  <div class="text-center">
+                     <img src="img/banners/weaver2.jpg" alt="" class="rounded-circle" height="200" width="200">
+                     
+                  </div>
+              </div><!-- ends: .col-lg-4 -->
+         <div class="col-lg-9">
+            <p>Dear Friends,</p>
+            <p>I believe that a design is made with art, craft and love. I love Indian wear: 
+               it’s dramatic silhouettes, large flares and huge surface area to work on. The diversity in Indian crafts and cultures have inspired me to choose Indian wear as my main stream of design. My strength lies in surface design and I like to challenge myself to include contemporary elements into Indian wear. I weave tradition with modern aesthetic to discover a new design statement for every collection and that has become the Brand’s Identity. Every design is intricately handcrafted from a white fabric to a fully finished
+                product. The label offers a range of artistic, elegant and comfortable clothing in Pret, Fusion and Bridal Wear..</p>
+         </div>
+         <div class="col-lg-12">
+            <p>
+                 Save the Weavers will now sell a line of products fashioned from the weavers’ cloth,
+                  providing the weavers with a steady income to support themselves and their families.</p>
+            <p>Thank you for supporting this great humanitarian cause.</p>
+         </div>
+      </div>
+   </div>
+</section>
+
+<?php include 'footer.php';?>
+
+
